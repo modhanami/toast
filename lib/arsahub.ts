336ee82d0ce566d5ef9e563b3fa7fd3b500dd522
Tawan Muadmuenwai <modhanami@gmail.com>
2024-04-01 21:14:08 +0700

    Extract ARSAHUB_URL

--- lib/arsahub.ts
+++ lib/arsahub.ts
@@ -1,2 +1,3 @@
 export const ARSAHUB_API_KEY = process.env.ARSAHUB_API_KEY || "";
 export const ARSAHUB_API_URL = process.env.ARSAHUB_API_URL || "";
+export const ARSAHUB_URL = process.env.ARSAHUB_URL || "";
