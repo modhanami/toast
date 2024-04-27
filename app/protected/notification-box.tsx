4bf61c547ecf21ab49704a2dac8fdde99b35fe7f
Tawan Muadmuenwai <modhanami@gmail.com>
2024-04-27 22:29:05 +0700

    Update layout

--- app/protected/notification-box.tsx
+++ app/protected/notification-box.tsx
@@ -15,145 +15,148 @@ let socket: Socket;
 export const IMAGE_BASE_URL = "https://ash-dev.modhanami.com";
 
 export function getImageUrlFromKey(key: string) {
-  const url = new URL(key, IMAGE_BASE_URL);
-  return url.toString();
+    const url = new URL(key, IMAGE_BASE_URL);
+    return url.toString();
 }
 
 export function NotificationBox() {
-  const [events, setEvents] = useState<WebhookEvent[]>([]);
-  const [user, setUser] = useState<User | null>(null);
+    const [events, setEvents] = useState<WebhookEvent[]>([]);
+    const [user, setUser] = useState<User | null>(null);
 
-  const supabaseClientClient = createClientClient();
+    const supabaseClientClient = createClientClient();
 
-  useEffect(() => {
-    (async () => {
-      await fetch("/api/socket");
+    useEffect(() => {
+        (async () => {
+            await fetch("/api/socket");
 
-      socket = io({
-        path: "/api/socket.io",
-      });
+            socket = io({
+                path: "/api/socket.io",
+            });
 
-      socket?.on("connect", () => {
-        console.log("Connected", socket.id);
-      });
+            socket?.on("connect", () => {
+                console.log("Connected", socket.id);
+            });
 
-    })();
+        })();
 
-    return () => {
-      socket?.close();
-    }
-  }, []);
+        return () => {
+            socket?.close();
+        }
+    }, []);
 
-  useEffect(() => {
-    supabaseClientClient.auth.getUser().then(({data}) => {
-      if (data.user) {
-        console.log("User", data.user);
-        setUser(data.user);
-      } else {
-        console.log("Not logged in");
-      }
-    });
-
-    // init events history
-    (async () => {
-      console.log("Fetching events")
-      const {
-        data: events,
-        error
-      } = await supabaseClientClient.from("webhook_events").select().order("created_at", {ascending: false}).limit(10).returns<WebhookEvent[]>();
-      if (error) {
-        console.error("Failed to fetch events", error);
-      } else {
-        console.log("Fetched events", events)
-        setEvents(events);
-      }
-    })()
-  }, []);
-
-  useEffect(() => {
-    if (user && socket) {
-      socket.emit("join", user.id);
-      socket.on("webhookEvent", (newEvent: WebhookEvent) => {
-        console.log("Webhook event", newEvent);
-        setEvents(events => {
-          const webhookIds = new Set(events.map(event => event.webhook_id));
-          console.log("Webhook IDs", webhookIds)
-          console.log("Event webhook ID", newEvent.webhook_id)
-
-          if (!("event" in newEvent.raw)) {
-            console.log("Invalid event", newEvent)
-            if (newEvent.raw.event === "points_updated") {
-              toast.success(`You earned ${newEvent.raw.payload.pointsChange} points!`);
+    useEffect(() => {
+        supabaseClientClient.auth.getUser().then(({data}) => {
+            if (data.user) {
+                console.log("User", data.user);
+                setUser(data.user);
+            } else {
+                console.log("Not logged in");
             }
-            if (newEvent.raw.event === "achievement_unlocked") {
-              const achievement = newEvent.raw.payload.achievement;
-              toast.success(`You unlocked the achievement: ${achievement.title}`);
+        });
+
+        // init events history
+        (async () => {
+            console.log("Fetching events")
+            const {
+                data: events,
+                error
+            } = await supabaseClientClient.from("webhook_events").select().order("created_at", {ascending: false}).limit(10).returns<WebhookEvent[]>();
+            if (error) {
+                console.error("Failed to fetch events", error);
+            } else {
+                console.log("Fetched events", events)
+                setEvents(events);
             }
-          }
+        })()
+    }, []);
+
+    useEffect(() => {
+        if (user && socket) {
+            socket.emit("join", user.id);
+            socket.on("webhookEvent", (newEvent: WebhookEvent) => {
+                console.log("Webhook event", newEvent);
+                setEvents(events => {
+                    const webhookIds = new Set(events.map(event => event.webhook_id));
+                    console.log("Webhook IDs", webhookIds)
+                    console.log("Event webhook ID", newEvent.webhook_id)
+
+                    if (!("event" in newEvent.raw)) {
+                        console.log("Invalid event", newEvent)
+                        if (newEvent.raw.event === "points_updated") {
+                            toast.success(`You earned ${newEvent.raw.payload.pointsChange} points!`);
+                        }
+                        if (newEvent.raw.event === "achievement_unlocked") {
+                            const achievement = newEvent.raw.payload.achievement;
+                            toast.success(`You unlocked the achievement: ${achievement.title}`);
+                        }
+                    }
 
 
-          if (!webhookIds.has(newEvent.webhook_id)) {
-            const latestTenEvents = [...events, newEvent]
-              .sort((a, b) => {
-                return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-              })
-              .slice(0, 10);
-            return latestTenEvents;
-          }
-          return events;
-        })
-      });
-    }
-  }, [user, socket]);
+                    if (!webhookIds.has(newEvent.webhook_id)) {
+                        const latestTenEvents = [...events, newEvent]
+                            .sort((a, b) => {
+                                return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+                            })
+                            .slice(0, 10);
+                        return latestTenEvents;
+                    }
+                    return events;
+                })
+            });
+        }
+    }, [user, socket]);
 
-  console.log("Events", events)
-  // const descendingEvents = events.sort((a, b) => {
-  //   return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-  // })
-  const descendingEvents = events
+    console.log("Events", events)
+    // const descendingEvents = events.sort((a, b) => {
+    //   return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+    // })
+    const descendingEvents = events
 
-  return (
-    <div>
-      <h1 className="text-lg font-medium">10 Latest Notifications</h1>
-      <ul>
-        {descendingEvents.map((event, index) => {
-          if (!("event" in event.raw)) {
-            console.log("Invalid event", event)
-            return null;
-          }
+    return (
+        <div>
+            <h1 className="text-lg font-medium">10 Latest Notifications</h1>
+            <ul>
+                {descendingEvents.map((event, index) => {
+                    if (!("event" in event.raw)) {
+                        console.log("Invalid event", event)
+                        return null;
+                    }
 
-          const timeAgo = dayjs(event.created_at).fromNow();
+                    const timeAgo = dayjs(event.created_at).fromNow();
 
-          if (event.raw.event === "points_updated") {
-            return (
-              <li key={index} className="flex items-center gap-4 p-1">
+                    if (event.raw.event === "points_updated") {
+                        return (
+                            <li key={index} className="flex items-center gap-4 p-1">
                 <span
-                  className="text-sm text-muted-foreground">{timeAgo}</span>
-                <p>You earned {event.raw.payload.pointsChange} points!</p>
-              </li>
-            )
-          }
+                    className="text-sm text-muted-foreground">{timeAgo}</span>
+                                <p>You earned {event.raw.payload.pointsChange} points!</p>
+                            </li>
+                        )
+                    }
 
-          if (event.raw.event === "achievement_unlocked") {
-            const achievement = event.raw.payload.achievement;
-            return (
-              <li key={index} className="flex items-center gap-4 p-1">
+                    if (event.raw.event === "achievement_unlocked") {
+                        const achievement = event.raw.payload.achievement;
+                        return (
+                            <li key={index} className="flex items-center gap-4 p-1">
                         <span
-                          className="text-sm text-muted-foreground">{timeAgo}</span>
-                <p>You unlocked the achievement: {achievement.title}</p>
-                <Image
-                  src={getImageUrlFromKey(achievement.imageKey)}
-                  alt={achievement.title!.toString()}
-                  width={50}
-                  height={50}
-                />
-              </li>
-            )
-          }
+                            className="text-sm text-muted-foreground">{timeAgo}</span>
+                                <p>You unlocked the achievement: {achievement.title}</p>
+                                <Image
+                                    src={getImageUrlFromKey(achievement.imageKey)}
+                                    alt={achievement.title!.toString()}
+                                    width={50}
+                                    height={50}
+                                />
+                            </li>
+                        )
+                    }
 
-          return null;
-        })}
-      </ul>
-    </div>
-  )
+                    return null;
+                })}
+                {descendingEvents.length === 0 && (
+                    <li className="text-muted-foreground">No notifications</li>
+                )}
+            </ul>
+        </div>
+    )
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -9,120 +9,131 @@ import {createServerClient} from "@/utils/supabase/server";
 import {NotificationBox} from "@/app/protected/notification-box";
 
 export default async function ProtectedPage() {
-  const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!);
-  const supabaseServerClient = createServerClient();
-
-  const {
-    data: {user},
-  } = await supabaseServerClient.auth.getUser();
-
-  if (!user) {
-    return redirect("/login");
-  }
-
-  const {data: linkedUser} = await supabaseAdmin.from('users').select().eq('user_id', user.id).maybeSingle<ToastUser>()
-  if (!linkedUser) {
-    // create arsahub user
-    const arsahubResponse = await fetch(`${ARSAHUB_API_URL}/apps/users`, {
-      method: "POST",
-      headers: {
-        "X-Api-Key": ARSAHUB_API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "uniqueId": user.id,
-        "displayName": user.email,
-      }),
-    });
-
-    const text = await arsahubResponse.text()
-    if (!arsahubResponse.ok) {
-      console.error("Failed to create user in arsahub", arsahubResponse.status, text);
-    }
-
-    // send trigger user_registered
-    const response = await fetch(`${ARSAHUB_API_URL}/apps/trigger`, {
-      method: "POST",
-      headers: {
-        "X-Api-Key": ARSAHUB_API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "key": "user_registered",
-        "userId": user.id,
-        "params": {}
-      }),
-    });
-
-    const triggerRegisteredResponse = await response.text()
-    console.log("triggerRegisteredResponse", response.status, triggerRegisteredResponse);
+    const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!);
+    const supabaseServerClient = createServerClient();
 
     const {
-      data: newUser,
-      error
-    } = await supabaseAdmin.from('users').insert({
-      user_id: user.id,
-      arsahub_onboarded_at: new Date(),
-    }).maybeSingle<ToastUser>()
+        data: {user},
+    } = await supabaseServerClient.auth.getUser();
 
-    if (error) {
-      console.error("Failed to create user in supabase", error);
-      return redirect("/");
-    } else {
-      console.log("Created new user", newUser);
+    if (!user) {
+        return redirect("/login");
     }
-  }
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        <div className="py-6 font-bold bg-primary-foreground text-center">
-          This is a protected page that you can only see as an authenticated
-          user
+    const {data: linkedUser} = await supabaseAdmin.from('users').select().eq('user_id', user.id).maybeSingle<ToastUser>()
+    if (!linkedUser) {
+        // create arsahub user
+        const arsahubResponse = await fetch(`${ARSAHUB_API_URL}/apps/users`, {
+            method: "POST",
+            headers: {
+                "X-Api-Key": ARSAHUB_API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "uniqueId": user.id,
+                "displayName": user.email,
+            }),
+        });
+
+        const text = await arsahubResponse.text()
+        if (!arsahubResponse.ok) {
+            console.error("Failed to create user in arsahub", arsahubResponse.status, text);
+        }
+
+        // send trigger user_registered
+        const response = await fetch(`${ARSAHUB_API_URL}/apps/trigger`, {
+            method: "POST",
+            headers: {
+                "X-Api-Key": ARSAHUB_API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "key": "user_registered",
+                "userId": user.id,
+                "params": {}
+            }),
+        });
+
+        const triggerRegisteredResponse = await response.text()
+        console.log("triggerRegisteredResponse", response.status, triggerRegisteredResponse);
+
+        const {
+            data: newUser,
+            error
+        } = await supabaseAdmin.from('users').insert({
+            user_id: user.id,
+            arsahub_onboarded_at: new Date(),
+        }).maybeSingle<ToastUser>()
+
+        if (error) {
+            console.error("Failed to create user in supabase", error);
+            return redirect("/");
+        } else {
+            console.log("Created new user", newUser);
+        }
+    }
+
+    return (
+        <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <div className="w-full">
+                <div className="py-6 font-bold bg-primary-foreground text-center">
+                    This is a protected page that you can only see as an authenticated
+                    user
+                </div>
+                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                    <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+                        <AuthButton/>
+                        <ModeToggle/>
+                    </div>
+                </nav>
+            </div>
+
+            <div className="flex gap-12 w-full p-4">
+                <div className="flex flex-col gap-8 items-end">
+
+                    <iframe
+                        className="h-[600px] w-[400px] border-2 border-primary-foreground rounded-lg overflow-hidden shadow-lg"
+                        src={`${ARSAHUB_URL}/embed/apps/${process.env.ARSAHUB_APP_ID}/users/${user.id}`}
+                        frameBorder="0"
+                    />
+
+                    <iframe
+                        className="h-[500px] w-[500px] border-2 border-primary-foreground rounded-lg overflow-hidden shadow-lg"
+                        src={`${ARSAHUB_URL}/embed/apps/${process.env.ARSAHUB_APP_ID}/leaderboard`}
+                        frameBorder="0"
+                    />
+
+                </div>
+
+                <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+                    <main className="flex-1 flex flex-col gap-6">
+                        <h2 className="font-bold text-4xl mb-2">Task List</h2>
+                        <p className="text-muted-foreground">
+                            You earn points by completing tasks. The more tasks you complete, the more points you earn.
+                            <br/>
+                            Some tasks also allow you to earn extra points by sharing your review.
+                        </p>
+                        <TaskList/>
+                    </main>
+                </div>
+
+                <NotificationBox/>
+            </div>
+
+
+            <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+                <p>
+                    Powered by{" "}
+                    <a
+                        href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                        target="_blank"
+                        className="font-bold hover:underline"
+                        rel="noreferrer"
+                    >
+                        Supabase
+                    </a>
+                </p>
+            </footer>
         </div>
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <AuthButton/>
-            <ModeToggle/>
-          </div>
-        </nav>
-      </div>
-
-      <NotificationBox/>
-
-      <div className="flex gap-4 w-[1000px]">
-        <iframe
-          className="w-1/2 h-96 border-2 border-primary-foreground rounded-lg overflow-hidden shadow-lg"
-          src={`${ARSAHUB_URL}/embed/apps/${process.env.ARSAHUB_APP_ID}/leaderboard`}
-          frameBorder="0"
-        />
-
-        <iframe
-          className="w-1/2 h-96 border-2 border-primary-foreground rounded-lg overflow-hidden shadow-lg"
-          src={`${ARSAHUB_URL}/embed/apps/${process.env.ARSAHUB_APP_ID}/users/${user.id}`}
-          frameBorder="0"
-        />
-      </div>
-
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6">
-          <TaskList/>
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-    </div>
-  );
+    );
 }
